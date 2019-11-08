edccd97b2099af46c143766d1b49a4941d15338c
bigtonito39 <bigtonito@hotmail.com>
2019-11-07 23:31:43 -0500

    Form validation created as well as Error Messaging, POST request is also now returning 201 Message!

--- user-onboarding/src/component/Form.js
+++ user-onboarding/src/component/Form.js
@@ -6,7 +6,7 @@ import * as Yup from "yup";
 
 
 const OnboardingForms = ({values, touched, errors}) => {
-
+console.log(values)
     return (
 <div className ="Onboarding-Forms">
  <Form>
@@ -16,21 +16,21 @@ const OnboardingForms = ({values, touched, errors}) => {
   placeholder="Name"
   value={values.name}  
 />
-
+{touched.name && errors.name && <p>{errors.name}</p>}
 <Field 
 type="text"
 name="email"
 placeholder="Email"
 value={values.email}
 />
-
+{touched.email && errors.email && <p>{errors.email}</p>}
 <Field 
 type="text"
 name="password"
 placeholder="Password"
 value={values.password}
 />
-
+{touched.password && errors.password && <p>{errors.password}</p>}
 <label> 
 Do you accept Terms of Use?
 <Field 
@@ -40,6 +40,7 @@ placeholder="Terms of Use"
 
 />
 </label>
+
 <button type="submit">Submit</button>
 </Form>                
 </div>
@@ -54,10 +55,31 @@ return {
  name: name || "",
  email: email || "",
  password: password || "",
- terms: terms || "",
+ terms: terms || ""
         
 };
 },
+
+validationSchema: Yup.object().shape({
+    name: Yup.string().required("Please please  your Name!"),
+    email: Yup.string().required("Please enter your Email!"),
+    password: Yup.string().required("please enter your Password!")
+     
+
+}),
+
+handleSubmit(values) {
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }  
+
+
 })(OnboardingForms);
 
 //always make sure to accept 
